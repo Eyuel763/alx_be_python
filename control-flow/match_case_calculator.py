18b8dbb6dfa1988642fe1d2879901ec5c5cbf410
Eyuel Gebreselassie <eyuelg1997@gmail.com>
2024-12-18 14:06:59 +0300

    Update match_case_calculator.py

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -4,17 +4,16 @@ operator = (input("Choose the operation (+, -, *, /): "))
 
 match operator:
         case "+":
-            result = first_number + second_number
+            print("The result is", first_number + second_number)
         case "-":
-            result = first_number - second_number
+            print("The result is", first_number - second_number)
         case "*":
-            result = first_number * second_number
+            print("The result is", first_number * second_number)
         case "/":
             if second_number == 0:
-                result = "Division by zero error"
+                print("Division by zero error")
             else:
-                result = first_number / second_number
+                print("The result is", first_number / second_number)
         case _:
-            result = "Invalid operator"
+            print("Invalid operator")
 
-print("The result is", result)
